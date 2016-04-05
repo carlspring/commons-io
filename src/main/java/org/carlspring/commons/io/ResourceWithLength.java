b815eda799b9598d3e3891f38b1573c571c24787
carlspring <carlspring@gmail.com>
2016-04-05 02:53:11 +0100

    - Extracted classes for reloading.

---
+++ src/main/java/org/carlspring/commons/io/ResourceWithLength.java
@@ -0,0 +1,11 @@
+package org.carlspring.commons.io;
+
+/**
+ * @author carlspring
+ */
+public interface ResourceWithLength
+{
+
+    long getLength();
+
+}
---
+++ src/main/java/org/carlspring/commons/io/reloading/Reloading.java
@@ -0,0 +1,14 @@
+package org.carlspring.commons.io.reloading;
+
+import java.io.IOException;
+
+/**
+ * @author mtodorov
+ */
+public interface Reloading
+{
+
+    void reload()
+            throws IOException;
+
+}
